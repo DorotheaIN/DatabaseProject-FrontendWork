17de5b2519be319a191e4bfe96ee5e547479ebf9
DiscreteL <1752962271@qq.com>
2021-06-14 11:08:56 +0800

    完成了登录功能

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -88,9 +88,14 @@ export default {
       this.$refs.loginFormRef.validate(async (valid) => {
         if (!valid) return;
         const { data: res } = await this.$http.post("login", this.loginForm); //替换"login"
-        if (res.meta.status !== 200) console.log("登录失败");
-        //替换"200"
-        else console.log("登录成功");
+        if (res.meta.status !== 200) //return this.$message.error("登录失败！");
+        this.$message.success("登录成功!");
+        // 1. 将登陆成功之后的token，保存到客户端的sessionStorage中
+        //  1.1 项目中除了登录之外的其他API接口，必须在登录之后才能访问
+        //  1.2 token只应在当前网站打开期间生效，所以将token保存在sessionStorage
+        window.sessionStorage.setItem("token",res.data.token);
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+        this.$router.push("/home")
       });
     },
     handleClick(tab, event) {
