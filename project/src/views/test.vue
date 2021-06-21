5572a19b3c61648716e5c4f7fc7ad1f5327e62a6
Tours <71580904+Di-Zayn@users.noreply.github.com>
2021-06-21 02:31:57 +0000

    Create test.vue
    测试界面

---
+++ project/src/views/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="test">
+    <docList></docList>
+  </div>
+</template>
+
+<script>
+import docList from '@/components/docList.vue'
+export default {
+  name:'Test',
+  components:{
+    docList
+  }
+}
+</script>
