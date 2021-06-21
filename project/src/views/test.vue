88f3ada63502226e728066ded309bf4dc973a473
Tours <71580904+Di-Zayn@users.noreply.github.com>
2021-06-21 09:50:34 +0000

    Update test.vue
    更新，该界面失效

--- project/src/views/test.vue
+++ project/src/views/test.vue
@@ -1,15 +1,12 @@
 <template>
   <div class="test">
-    <docList></docList>
   </div>
 </template>
 
 <script>
-import docList from '@/components/docList.vue'
 export default {
   name:'Test',
   components:{
-    docList
+    }
   }
-}
 </script>
