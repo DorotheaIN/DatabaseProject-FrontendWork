2abfb2368622b62cbcd8ed0af4b3bf5a45e56187
Tours <71580904+Di-Zayn@users.noreply.github.com>
2021-06-21 09:53:32 +0000

    Update doc.js
    为方便组件封装，做了一些改动

--- project/mocks/response/doc.js
+++ project/mocks/response/doc.js
@@ -8,12 +8,12 @@ export const getDocList = () => {
     for (let i = 1; i <= 48; i++) {
 
         let newData = {
-            "docId":i,
-            "docInfo":{
-                "docName": Random.cname(),
-                "docAge":Random.natural(25,65),
-                "docLevel|1":["主任医师","副主任医师","主治医师","住院医师"],
-                "docProfession|1":["皮肤科","内科","消化科"]
+            "id":i,
+            "info":{
+                "name": Random.cname(),
+                "age":Random.natural(25,65),
+                "level|1":["主任医师","副主任医师","主治医师","住院医师"],
+                "profession|1":["皮肤科","内科","消化科"]
             },
         }
         docList.push(newData)
