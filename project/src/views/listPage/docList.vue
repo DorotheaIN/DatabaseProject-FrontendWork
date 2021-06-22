3fdbff09a0c870625c2aa33abe9b1741d717caf6
Tours <71580904+Di-Zayn@users.noreply.github.com>
2021-06-22 07:49:59 +0000

    Update docList.vue
    添加了一种筛选器 修改了josn格式

--- project/src/views/listPage/docList.vue
+++ project/src/views/listPage/docList.vue
@@ -1,60 +1,78 @@
 <template>
   <div class="docList">
     <div>
-        <myselect :selectedOptions="options" :selectedData="docList" @selectData="changeData">
-        </myselect>
+      <myselect
+        :selectedOptions="options"
+        :selectedData="docList"
+        @selectData="changeData"
+      >
+      </myselect>
     </div>
     <div>
-        <cardList :dataList="docData">
-        </cardList>
+      <cardList :dataList="docData"> </cardList>
     </div>
-    
   </div>
 </template>
 
 <script>
-import cardList from '@/components/cardList.vue'
-import myselect from '@/components/singleSelect.vue'
+import cardList from "@/components/cardList.vue";
+//import myselect from "@/components/singleSelect.vue";
+import myselect from '@/components/searchSelect.vue'
 export default {
-    name:'DocList',
-    components:{
-        cardList,
-        myselect
-        },
-    data(){
-         return {
-             docList:[],
-             docData:[],
-             options:[],
-             }
+  name: "DocList",
+  components: {
+    cardList,
+    myselect,
+  },
+  data() {
+    return {
+      docList: [],
+      docData: [],
+      options: [],
+      //optionList:[],
+    };
+  },
+  created() {
+    this.getDataList();
+    this.getOptions();
+  },
+//   beforeUpdate(){//因为涉及$所以不能在created中 mounted也不行...
+//     this.formOptions();
+//     console.log("刷新");
+//   },
+  methods: {
+    getDataList() {
+      this.$http
+        .get("/data/userList.josn")
+        .then((res) => {
+          this.docList = res.data;
+          this.docData = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    created(){
-        this.getDataList();
-        this.getOptions();
+    getOptions() {
+      this.$http
+        .get("/data/selectOption.josn")
+        .then((res) => {
+          //this.options = res.data;
+          this.options = res.data.map((item) => {
+          return { value: `${item}`, label: `${item}` };
+          });
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     },
-    methods:{
-        getDataList(){
-            this.$http.get("/data/userList.josn")
-            .then((res) => {
-                this.docList = res.data;
-                this.docData = res.data;
-            })
-            .catch((err) => {
-                console.log(err);
-                });
-        },
-        getOptions(){
-            this.$http.get("/data/selectOption.josn")
-            .then((res) => {
-                this.options = res.data;
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        },
-        changeData(newdata){
-            this.docData = newdata
-        }
+    // formOptions() {
+    //   this.optionList = this.options.map((item) => {
+    //       return { value: `${item}`, label: `${item}` };
+    //       });
+    // },
+    changeData(newdata) {
+      this.docData = newdata;
     },
-}
+  },
+};
 </script>
